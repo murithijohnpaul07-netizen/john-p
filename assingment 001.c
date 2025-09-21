d8f694fb51d4924c026d7abe706a7d349edd85f5
murithijohnpaul07-netizen <murithijohnpaul07@gmail.com>
2025-09-21 20:31:55 +0300

    Add files via upload

---
+++ assingment 001.c
@@ -0,0 +1,24 @@
+/*
+Name  :john paul murithi
+Reg No:CT101/G/26544/25
+Date  :21/09/2025
+*/
+
+#include<stdio.h>
+int main()
+{  
+    int age;
+	double income;
+	
+	printf("Enter your age:");
+	scanf("%d", &age);
+	
+	printf("Enter your income:");
+	scanf("%lf", income);
+	
+    if(age>=21 &income>=21000)
+	{printf("congratulation,you qualifly for a loan.\n");} else 
+	{printf("unfortunately,we are unable to offer you a loan at this time.\n");}
+	
+	retern 0
+}
---
+++ assingment 002.c
@@ -0,0 +1,29 @@
+/*
+Name  :John paul murithi
+Reg No:CT101/G/26544/25
+Date  :21/09/2025
+*/
+
+#include<stdio.h> 
+#define PI3.141593654
+int main ()
+{
+	float r, h, Volume, Surface area;
+
+// input: radius & height
+	printf("Enter the radius of the cylinder:");
+	scanf("%f", &r);
+	
+	printf("Enter the height of the cylinder:");
+	scanf("%f", &h);
+
+//formulas; volume & surface area	
+	Volume=(PI*r*r*h);
+	Surface area=(2*PI*r*r)+(2*PI*r*h);
+	
+//output result,		
+	printf("Volume of the cylinder:%2f\n",volume);
+	printf("Surface area of the cylinder:%2f\n",Surface area);
+	
+	return 0
+}
