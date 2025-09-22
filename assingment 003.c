d7832aea7da1c61cfbb453a22f7d9a4e564252e3
murithijohnpaul07-netizen <murithijohnpaul07@gmail.com>
2025-09-22 19:46:06 +0300

    Add files via upload

---
+++ assingment 003.c
@@ -0,0 +1,20 @@
+/*
+Name  :John paul murithi
+Reg No:CT101/G/26544/25
+Date  :12/09/2025
+*/
+
+#include<stdio.h>
+int main()
+{
+	printf("SPC 2101 Introduction to programming and problem solving\n");
+	printf("SPC 2102 Introduction to computer Architucture\n");
+	printf("SPH 2101 Physics\n");
+	printf("SPH 2107 Analoge electronics\n");
+	printf("SPM 2100 Discete Mathemactics\n");
+	printf("SPM 2104 Mathemactics for science\n");
+	printf("UCU 2105 Fundermental of ICT\n");
+	printf("UCU 2106 Communication and information literacy skills\n");
+
+	return 0;
+}
