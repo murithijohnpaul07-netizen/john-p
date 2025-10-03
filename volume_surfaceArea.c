3c0fdf3cf0bba82bd1b1778fe823223558590f9d
murithijohnpaul07-netizen <murithijohnpaul07@gmail.com>
2025-10-03 13:57:00 +0300

    Update volume_surfaceArea.c

--- volume_surfaceArea.c
+++ volume_surfaceArea.c
@@ -5,12 +5,11 @@ Date  :21/09/2025
 desciption:Volume_surfaceArea_calculation
 */
 
-#include<stdio.h> 
-#define PI3.141593654
+#include <stdio.h> 
 int main ()
 {
-	float r, h, Volume, Surface area;
-
+	float r, h, Volume, Surface_area;
+     float PI=3.141593654;
 // input: radius & height
 	printf("Enter the radius of the cylinder:");
 	scanf("%f", &r);
@@ -20,13 +19,14 @@ int main ()
 
 //formulas; volume & surface area	
 	Volume=(PI*r*r*h);
-	Surface area=(2*PI*r*r)+(2*PI*r*h);
+	Surface_area=(2*PI*r*r)+(2*PI*r*h);
 	
 //output result,		
-	printf("Volume of the cylinder:%2f\n",volume);
-	printf("Surface area of the cylinder:%2f\n",Surface area);
+	printf("Volume of the cylinder:%2f\n",Volume);
+	printf("Surface_area of the cylinder:%2f\n",Surface_area);
 	
 	return 0;
 
 }
+
 
